5fe8d1ed5f48b8552fb21f0530f587fb88e8c2ec
BBrownley <brendobrownley@yahoo.com>
2021-08-16 04:01:56 -0400

    Initial project setup

---
+++ client/src/theme.js
@@ -0,0 +1,11 @@
+const colors = {
+  primary: "#D87D4A",
+  primaryLight: "#FBAF85",
+  white700: "#FFFFFF",
+  white600: "#FAFAFA",
+  white500: "#F1F1F1",
+  black700: "#000000",
+  black600: "#101010"
+}
+
+export default colors;
