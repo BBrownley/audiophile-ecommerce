1a5762914e520b38d5df0d45b0034fb77fdb9123
BBrownley <brendobrownley@yahoo.com>
2021-08-16 04:21:02 -0400

    Setting up react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Home from "./components/Home/Home";
+import Headphones from "./components/Headphones/Headphones";
+import Speakers from "./components/Speakers/Speakers";
+import Earphones from "./components/Earphones/Earphones";
 
 function App() {
   return (
     <div className="App">
-      <p>hi :)</p>
+      <Router>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/headphones">
+          <Headphones />
+        </Route>
+        <Route path="/speakers">
+          <Speakers />
+        </Route>
+        <Route path="/earphones">
+          <Earphones />
+        </Route>
+        <ul>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/headphones">headphones</Link>
+          </li>
+          <li>
+            <Link to="/speakers">speakers</Link>
+          </li>
+          <li>
+            <Link to="/earphones">earphones</Link>
+          </li>
+        </ul>
+      </Router>
     </div>
   );
 }
